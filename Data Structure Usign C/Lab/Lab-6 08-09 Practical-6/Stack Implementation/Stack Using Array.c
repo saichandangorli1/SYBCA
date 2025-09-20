c73cd34450ae80b3513ffa9c25efc3dca0a5edf3
saichandangorli1 <gorlisaichandan14@gmail.com>
2025-09-21 00:13:37 +0530

    Fixed typo

---
+++ Data Structure Usign C/Lab/Lab-6 08-09 Practical-6/Stack Implementation/Stack Using Array.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+
+typedef struct Stack
+{
+    int top;
+    int array[MAX_SIZE];
+} *stack;
+
+void initStack(stack s)
+{
+    s->top = -1;
+}
+
+int isFull(stack s)
+{
+    return s->top == MAX_SIZE - 1;
+}
+
+int isEmpty(stack s)
+{
+    return s->top == -1;
+}
+
+void push(stack s, int value)
+{
+    if (isFull(s))
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+    s->array[++(s->top)] = value;
+    printf("%d pushed to stack\n", value);
+}
+
+int pop(stack s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    return s->array[(s->top)--];
+}
+
+int peek(stack s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack is Empty\n");
+        return -1;
+    }
+    return s->array[s->top];
+}
+
+void display(stack s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack is Empty\n");
+        return;
+    }
+    for (int i = s->top; i >= 0; i--)
+    {
+        printf("%d\n", s->array[i]);
+    }
+}
+
+int main()
+{
+    stack s = (stack)malloc(sizeof(struct Stack));
+    initStack(s);
+
+    push(s, 10);
+    push(s, 20);
+    push(s, 30);
+
+    printf("%d popped from stack\n", pop(s));
+    printf("Top element is %d\n", peek(s));
+
+    display(s);
+
+    free(s); // Remember to free the allocated memory
+    return 0;
+}
---
+++ Data Structure Usign C/Lab/Lab-6 08-09 Practical-6/Stack Implementation/Stack Using LinkedList.c
@@ -0,0 +1,93 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+
+// push,pop,seek,displauy
+
+struct Stack
+{
+    int data;
+    struct Stack *next;
+};
+
+int count = 0;
+
+typedef struct Stack *node;
+
+void push(node *top, int data)
+{
+    node new = (node)malloc(sizeof(struct Stack));
+    if (new == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    if (count == MAX)
+    {
+        printf("Stack Overflow\n");
+        printf("%d is not pushed\n", data);
+        return;
+    }
+    new->data = data;
+    new->next = *top;
+    *top = new;
+    count++;
+    printf("%d pushed to stack\n", data);
+}
+
+int pop(node *top)
+{
+    if (*top == NULL)
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    node temp = *top;
+    int poppedData = temp->data;
+    *top = temp->next;
+    free(temp);
+    count--;
+    return poppedData;
+}
+
+int peek(node top)
+{
+    if (top == NULL)
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    return top->data;
+}
+
+void display(node top)
+{
+    if (top == NULL)
+    {
+        printf("Stack is Empty\n");
+        return;
+    }
+    node temp = top;
+    printf("Stack elements:\n");
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    node top = NULL;
+
+    push(&top, 10);
+    push(&top, 20);
+    push(&top, 30);
+    push(&top, 40);
+    push(&top, 50);
+    push(&top, 60); // This should show stack overflow
+
+    display(top);
+    return 0;
+}
